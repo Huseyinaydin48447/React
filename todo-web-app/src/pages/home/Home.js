89e3fd669276b5d9d196845cff7a3a85d5924fc4
Huseyinaydin48447 <109576958+Huseyinaydin48447@users.noreply.github.com>
2024-05-23 13:29:55 +0300

    update
    update . =>23.05.2024

--- todo-web-app/src/pages/home/Home.js
+++ todo-web-app/src/pages/home/Home.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
-import { Table } from 'react-bootstrap';
+import { Table, Spinner, Pagination } from 'react-bootstrap';
 import axios from 'axios';
 import CreateTodoModal from '../../components/model/CreateTodoModal';
 import DeleteTodoModal from '../../components/model/DeleteTodoModal';
 import UpdateTodoModal from '../../components/model/UpdateTodoModal';
-import PDFDownloader from '../../components/PDFDownloader/PDFDownloader'; 
+import PDFDownloader from '../../components/PDFDownloader/PDFDownloader';
 import secureLocalStorage from 'react-secure-storage';
-
 const Home = () => {
   const GetToken = JSON.parse(secureLocalStorage.getItem('userData'));
   const [todoList, setTodoList] = useState([]);
@@ -16,8 +15,11 @@ const Home = () => {
   const [showAddForm, setShowAddForm] = useState(false);
   const [showDeleteModal, setShowDeleteModal] = useState(false);
   const [showUpdateModal, setShowUpdateModal] = useState(false);
-  const [isDeleting, setIsDeleting] = useState(false); 
+  const [isDeleting, setIsDeleting] = useState(false);
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [loadingCheck, setLoadingCheck] = useState({});
+  const [currentPage, setCurrentPage] = useState(1);
+  const todosPerPage = 5; 
 
   useEffect(() => {
     fetchData();
@@ -31,7 +33,6 @@ const Home = () => {
     setShowUpdateModal(false);
   };
 
-  // Fetch data
   const fetchData = async () => {
     try {
       const response = await axios.get(`${process.env.REACT_APP_API_ENDPOINT}/getAll`, {
@@ -40,71 +41,65 @@ const Home = () => {
         }
       });
       setTodoList(response.data.todoList);
+      
+
     } catch (error) {
       console.error('Error fetching todo list:', error);
     }
   };
 
-  // Handle edit
   const handleEdit = (todo) => {
     setSelectedTodo(todo);
     setFormData({ text: todo.text, time: todo.time });
     setShowUpdateModal(true);
   };
 
-  // Handle change
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const getCurrentDate = ()=> {
+  const getCurrentDate = () => {
     const sysTime = new Date();
-    const fullDate =  `${sysTime.getDate()}/${sysTime.getMonth()+1}/${sysTime.getFullYear()}`
-    //console.log(`${sysTime.getDate()}/${sysTime.getMonth()+1}/${sysTime.getFullYear()}`)
+    const fullDate = `${sysTime.getDate()}/${sysTime.getMonth() + 1}/${sysTime.getFullYear()}`;
     return fullDate;
   }
 
-  // Handle submit
-// Handle submit
-const handleSubmit = async (e) => {
-  e.preventDefault();
-  setIsSubmitting(true); 
-  try {
-    const config = {
-      method: selectedTodo ? 'put' : 'post',
-      url: selectedTodo
-      ? `${process.env.REACT_APP_API_ENDPOINT}/update/${selectedTodo.id}`
-      : `${process.env.REACT_APP_API_ENDPOINT}/create`,
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${GetToken?.JWTAccessToken}`
-      },
-      data: {
-        text: formData.text,
-        time: formData.time ? formData.time : getCurrentDate()
-      }
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+    try {
+      const config = {
+        method: selectedTodo ? 'put' : 'post',
+        url: selectedTodo
+          ? `${process.env.REACT_APP_API_ENDPOINT}/update/${selectedTodo.id}`
+          : `${process.env.REACT_APP_API_ENDPOINT}/create`,
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${GetToken?.JWTAccessToken}`
+        },
+        data: {
+          text: formData.text,
+          time: formData.time ? formData.time : getCurrentDate()
+        }
+      };
 
-    const response = await axios(config);
-    console.log(response.data);
-    setShowUpdateModal(false);
-    setShowAddForm(false);
-    setFormData({ text: '', time: null }); 
-    fetchData();
-  } catch (error) {
-    console.error('Error updating/creating todo:', error);
-    alert('Bir hata oluştu, lütfen tekrar deneyin.');
-    
-  } finally {
-    setIsSubmitting(false); 
-  }
-};
+      const response = await axios(config);
+      console.log(response.data);
+      setShowUpdateModal(false);
+      setShowAddForm(false);
+      setFormData({ text: '', time: null });
+      fetchData();
+    } catch (error) {
+      console.error('Error updating/creating todo:', error);
+      alert('Bir hata oluştu, lütfen tekrar deneyin.');
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
 
-
-  // Handle delete
   const handleDelete = async (todoId) => {
-    if (isDeleting) return; 
-    setIsDeleting(true); 
+    if (isDeleting) return;
+    setIsDeleting(true);
 
     try {
       const config = {
@@ -122,7 +117,7 @@ const handleSubmit = async (e) => {
     } catch (error) {
       console.error('Error deleting todo:', error);
     } finally {
-      setIsDeleting(false); 
+      setIsDeleting(false);
       setShowDeleteModal(false);
     }
   };
@@ -137,15 +132,15 @@ const handleSubmit = async (e) => {
   const handleAddButtonClick = () => {
     setShowAddForm(true);
     setFormData({ text: '', time: null });
-
   };
 
   const handleCheck = async (todoId, isChecked) => {
+    setLoadingCheck(prevState => ({ ...prevState, [todoId]: true }));
     try {
       const data = {
         checked: isChecked
       };
-  
+
       const config = {
         method: 'put',
         url: `${process.env.REACT_APP_API_ENDPOINT}/updateCheck/${todoId}`,
@@ -155,19 +150,33 @@ const handleSubmit = async (e) => {
         },
         data: data
       };
-  
+
       const response = await axios(config);
       console.log(response.data);
       fetchData();
     } catch (error) {
       console.error('Error updating todo status:', error);
+    } finally {
+      setLoadingCheck(prevState => ({ ...prevState, [todoId]: false }));
     }
   };
 
+  const indexOfLastTodo = currentPage * todosPerPage;
+  const indexOfFirstTodo = indexOfLastTodo - todosPerPage;
+  const currentTodos = todoList.slice(indexOfFirstTodo, indexOfLastTodo);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  const totalTodos = todoList.length;
+  const completedTodos = todoList.filter(todo => todo.checked).length;
+
+
+
   return (
     <>
+    
       <Button variant="warning me-2" onClick={handleAddButtonClick}>Add</Button>
-      <PDFDownloader tableData={todoList} /> 
+      <PDFDownloader tableData={todoList} />
       <CreateTodoModal
         show={showAddForm}
         handleClose={() => setShowAddForm(false)}
@@ -176,40 +185,59 @@ const handleSubmit = async (e) => {
         handleSubmit={handleSubmit}
         handleCancel={handleCancel}
         isSubmitting={isSubmitting}
-
       />
 
       <Table striped bordered hover>
         <thead>
           <tr>
             <th></th>
-            <th>text</th>
-            <th>time</th>
+            <th>Text</th>
+            <th>Time</th>
             <th>Action</th>
           </tr>
         </thead>
         <tbody>
-        {todoList.map((todo, index) => (
-          <tr key={index}>
-            <td>
-              <input
-                type="checkbox"
-                checked={todo.checked} 
-                onChange={(e) => handleCheck(todo.id, e.target.checked)}
-              />
-            </td>
-            <td>{todo.text}</td>
-            <td>{todo.time}</td>
-            <td style={{ display: 'flex', justifyContent: 'flex-end', alignContent: 'flex-end' }}>
-              <span style={{ justifyContent: 'space-evenly' }}>
-                <Button variant="success me-2 " onClick={() => handleEdit(todo)}>Edit</Button>
+          {currentTodos.map((todo, index) => (
+            <tr key={index}>
+              <td>
+                {loadingCheck[todo.id] ? (
+                  <Spinner animation="border" size="sm" />
+                ) : (
+                  <input
+                    type="checkbox"
+                    checked={todo.checked}
+                    onChange={(e) => handleCheck(todo.id, e.target.checked)}
+                  />
+                )}
+              </td>
+              <td style={{ textDecoration: todo.checked ? 'line-through' : 'none' }}>{todo.text}</td>
+              <td style={{ textDecoration: todo.checked ? 'line-through' : 'none' }}>{todo.time}</td>
+              <td style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
+                <Button variant="success me-2" onClick={() => handleEdit(todo)}>Edit</Button>
                 <Button variant="danger" onClick={() => { setSelectedTodo(todo); setShowDeleteModal(true); }}>Delete</Button>
-              </span>
-            </td>
-          </tr>
-        ))}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </Table>
+
+      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+  <div>
+    <span>Total: {totalTodos}</span> | <span>Completed: {completedTodos}</span>
+  </div>
+  <div style={{ display: 'flex', alignItems: 'center' }}>
+    <Pagination>
+      <Pagination.Prev onClick={() => paginate(currentPage - 1)} disabled={currentPage === 1} />
+      {Array.from({ length: Math.ceil(todoList.length / todosPerPage) }, (_, index) => (
+        <Pagination.Item key={index + 1} active={index + 1 === currentPage} onClick={() => paginate(index + 1)}>
+          {index + 1}
+        </Pagination.Item>
+      ))}
+      <Pagination.Next onClick={() => paginate(currentPage + 1)} disabled={currentPage === Math.ceil(todoList.length / todosPerPage)} />
+    </Pagination>
+  </div>
+</div>
+
 
       <DeleteTodoModal
         show={showDeleteModal}
